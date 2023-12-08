735f45ab6e6d6c3fd2f1c6eb30c5ec0ab4682952
m1hN <145584972+m1hN@users.noreply.github.com>
2023-12-08 10:00:48 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -27,10 +27,10 @@ function showGrade() {
   // Output
   document.getElementById("outcome").innerHTML = calculateGrade;
 
-  if (calculateGrade <= 80) {
+  if (calculateGrade < 80) {
     document.getElementById("statement").innerHTML =
       "You have to improve. Study hard, eat healthy, drink your fluids. I'm VERY disappointed.";
-  } else if (80 <= calculateGrade < 100) {
+  } else if (calculateGrade >= 80 && calculateGrade < 100) {
     document.getElementById("statement").innerHTML =
       "Good job, but not a 100%? Pathetic.";
   } else if (calculateGrade == 100) {
@@ -40,3 +40,14 @@ function showGrade() {
     document.getElementById("statement").innerHTML = "Invalid try again.";
   }
 }
+
+// let x = 10;
+// let y = 0;
+
+// if (x == 10) {
+//   console.log('x is 10');
+// } else if (y < -5) {
+//   console.log('y less than -5');
+// } else if (x == 10 && y < 10) {
+//   console.log('blah');
+// } else if (x > 0 && x < 10)  // 0 < x < 10
